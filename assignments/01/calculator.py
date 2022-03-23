ee4f67471f30bf68508a275d9ce47c1c35989b01
thegrimfox <101572716+thegrimfox@users.noreply.github.com>
2022-03-23 14:19:16 -0300

    ah

--- assignments/01/calculator.py
+++ assignments/01/calculator.py
@@ -4,76 +4,98 @@ import typer
 app = typer.Typer()
 
 
+class calculadora():
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def suma(a, b):
+        """
+        in this part the sum of a and b will be done
+
+        parameters
+        -----
+        a: int
+            first number of the sum
+        b: int
+            the second number of the sum
+
+        returns
+        int
+            the sum of a and b
+        """
+        return a + b
+
+    @staticmethod
+    def resta(a, b):
+        """
+        in this part the subtraction of a and b will be done
+
+
+        parameters:
+            a: int
+                first number of the subtraction
+            b:int
+                second number of the subtraction
+        returns
+        int
+            the subtraction of a and b
+        """
+        return a - b
+
+    @staticmethod
+    def division(a, b):
+        """
+        In this part, the division of a into b will be done
+
+
+        parameters:
+            a: int
+                dividend
+            b:int
+                divider
+        returns
+        int
+            the division between a and b
+        """
+        return a / b
+
+    @staticmethod
+    def multiplicar(a, b):
+        """
+        In this part, the multiplication between a and b will be done
+
+        parameters:
+            a: int
+                first number of the multiplication
+            b:int
+                second number of the multiplication
+        returns
+        int
+            the multiplication of a and b
+        """
+        return a * b
+
+
 @app.command()
 def suma(a: int, b: int):
-    """
-    in this part the sum of a and b will be done
-
-    parameters
-    -----
-    a: int
-        first number of the sum
-    b: int
-        the second number of the sum
-
-    returns
-    int
-        the sum of a and b
-    """
-    print(f'sum of a + b = {a+b}')
-
+    res = calculadora.suma(a, b)
+    print(f'la suma entre {a} y {b} es de {res}')
 
 @app.command()
 def resta(a: int, b: int):
-    """
-     in this part the subtraction of a and b will be done
+    res = calculadora.resta(a, b)
+    print(f'la resta entre {a} y {b} es de {res}')
 
-
-    parameters:
-        a: int
-            first number of the subtraction
-        b:int
-            second number of the subtraction
-    returns
-    int
-        the subtraction of a and b
-    """
-    print(f'sustraction of a - b = {a-b}')
-
+@app.command()
+def multiplicacion(a: int, b: int):
+    res = calculadora.multiplicar(a, b)
+    print(f'la multiplicacion entre {a} y {b} es de {res}')
 
 @app.command()
 def division(a: int, b: int):
-    """
-    In this part, the division of a into b will be done
-
-
-    parameters:
-        a: int
-            dividend
-        b:int
-            divider
-    returns
-    int
-        the division between a and b
-    """
-    print(f'division of a / b = {a/b}')
-
-
-@app.command()
-def multiplicar(a: int, b: int):
-    """
-    In this part, the multiplication between a and b will be done
-
-    parameters:
-        a: int
-            first number of the multiplication
-        b:int
-            second number of the multiplication
-    returns
-    int
-        the multiplication of a and b
-    """
-    print(f'multiplication of a * b = {a*b}')
+    res = calculadora.division(a, b)
+    print(f'la division entre {a} y {b} es de {res}')
 
 
 def main():
---
+++ assignments/02/time.py
@@ -0,0 +1,79 @@
+import time
+
+
+def count_to(number=1000):
+    for _ in range(number):
+        time.sleep(0.001)
+
+
+class Timer():
+
+    def __init__(self, unit):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        unit : str
+            Time unit [s|ms]
+        """
+        # -----------------------------
+        # Add code here (start)
+        # -----------------------------
+        pass
+        # -----------------------------
+        # Add code here (end)
+        # -----------------------------
+
+    def measure_time(self, method):
+        """
+        Measure the time that takes to run a given method (function)
+
+        Parameters
+        ----------
+        method : function
+            Given method to measure
+
+        Returns
+        -------
+        float
+            The elapsed time in the unit from constructor
+        """
+        # -----------------------------
+        # Add code here (start)
+        # -----------------------------
+        pass
+        # -----------------------------
+        # Add code here (end)
+        # -----------------------------
+
+    def get_unit(self):
+        """
+        Returns the selected time measurement unit
+
+        Returns
+        -------
+        str:
+            Selected unit
+
+        """
+        # -----------------------------
+        # Add code here (start)
+        # -----------------------------
+        pass
+        # -----------------------------
+        # Add code here (end)
+        # -----------------------------
+
+
+def main():
+    t = Timer('ms')
+    elapsed = t.measure_time(count_to)
+    print(f'Elapsed time: {elapsed} {t.get_unit()}')
+    t = Timer('s')
+    elapsed = t.measure_time(count_to)
+    print(f'Elapsed time: {elapsed} {t.get_unit()}')
+
+
+if __name__ == '__main__':
+    main()
